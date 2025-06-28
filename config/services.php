d447612ad5a18d7b70632e95e5e6136811419df4
DanielBello <danbello1998@gmail.com>
2025-06-28 17:40:57 -0500

    Init commit

---
+++ config/services.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
+    ],
+
+    'resend' => [
+        'key' => env('RESEND_KEY'),
+    ],
+
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+
+    'slack' => [
+        'notifications' => [
+            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
+            'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
+        ],
+    ],
+
+    'football_data' => [
+        'api_key' => env('FOOTBALL_DATA_API_KEY'),
+        'api_url' => env('FOOTBALL_DATA_API_URL', 'https://api.football-data.org/v4'),
+        'rate_limit' => env('FOOTBALL_DATA_RATE_LIMIT', 10),
+        'timeout' => env('FOOTBALL_DATA_TIMEOUT', 30),
+    ],
+];
